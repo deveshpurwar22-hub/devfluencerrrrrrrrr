33cfebc58f9c1e5125412679a2460013e4da74ba
deveshpurwar22-hub <deveshpurwar22@gmail.com>
2026-07-09 08:37:42 +0530

    Update Home.tsx

--- Home.tsx
+++ Home.tsx
@@ -1,33 +1,12 @@
-import { Navbar } from "../sections/Navbar";
-import { Hero } from "../sections/Hero";
-import { Portfolio } from "../sections/Portfolio";
-import { Problem } from "../sections/Problem";
-import { System45 } from "../sections/System45";
-import { Process } from "../sections/Process";
-import { Services } from "../sections/Services";
-import { WhoWeHelp } from "../sections/WhoWeHelp";
-import { Founder } from "../sections/Founder";
-import { FinalCTA } from "../sections/FinalCTA";
-import { Footer } from "../sections/Footer";
-import { WhatsAppFloat } from "../components/WhatsAppFloat";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col min-h-screen bg-[#080808]">
-      <Navbar />
-      <main className="flex-1">
-        <Hero />
-        <Portfolio />
-        <Problem />
-        <System45 />
-        <Process />
-        <Services />
-        <WhoWeHelp />
-        <Founder />
-        <FinalCTA />
-      </main>
-      <Footer />
-      <WhatsAppFloat />
-    </div>
-  );
-}
+import { Navbar } from "./Navbar";
+import { Hero } from "./Hero";
+import { Portfolio } from "./Portfolio";
+import { Problem } from "./Problem";
+import { System45 } from "./System45";
+import { Process } from "./Process";
+import { Services } from "./Services";
+import { WhoWeHelp } from "./WhoWeHelp";
+import { Founder } from "./Founder";
+import { FinalCTA } from "./FinalCTA";
+import { Footer } from "./Footer";
+import { WhatsAppFloat } from "./WhatsAppFloat";
