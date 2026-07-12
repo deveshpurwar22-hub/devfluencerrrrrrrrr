8fc909d3d0e58f69ae963bb9b452d3ecbfd65901
deveshpurwar22-hub <deveshpurwar22@gmail.com>
2026-07-12 15:49:22 +0530

    Update Navbar.tsx

--- Navbar.tsx
+++ Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
-import logo from "./logo-dark-trimmed.png";
+import logo from "./Devfluencer-01.png";
 
 const WhatsAppIcon = () => (
   <svg
